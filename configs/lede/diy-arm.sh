ad51c64fb41f87f7c12c86ea6463d53bf1946212
dfbucy090 <70613514+dfbucy090@users.noreply.github.com>
2024-03-04 11:01:45 +0800

    Update diy-arm.sh

--- configs/lede/diy-arm.sh
+++ configs/lede/diy-arm.sh
@@ -40,6 +40,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/o
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 git clone -b dev --single-branch --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
+rm -rf package/helloworld/gn
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 # git clone -b master --single-branch --depth=1 https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
