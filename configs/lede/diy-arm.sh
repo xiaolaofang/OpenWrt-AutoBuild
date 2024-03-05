7618dca997a21788aff5c764fe03a1ef912d0c2b
dfbucy090 <70613514+dfbucy090@users.noreply.github.com>
2024-03-05 19:55:46 +0800

    Update diy-arm.sh

--- configs/lede/diy-arm.sh
+++ configs/lede/diy-arm.sh
@@ -34,16 +34,16 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 
 # Add additional packages
 # git clone -b main --single-branch --depth=1 https://github.com/dfbucy090/helloworld.git package/helloworld
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+# git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-git clone -b dev --single-branch --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
+# git clone -b dev --single-branch --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-# git clone -b master --single-branch --depth=1 https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-# git clone -b master --single-branch --depth=1 https://github.com/kenzok8/small.git package/small
+git clone -b master --single-branch --depth=1 https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone -b master --single-branch --depth=1 https://github.com/kenzok8/small.git package/small
 
 # dockerd去版本验证
 #sed -i 's/^\s*$[(]call\sEnsureVendoredVersion/#&/' feeds/packages/utils/dockerd/Makefile
