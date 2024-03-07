14722fd5528a0eefb6ee4459fb7ea0eb474db4e4
dfbucy090 <70613514+dfbucy090@users.noreply.github.com>
2024-03-08 07:35:57 +0800

    Update diy-arm.sh

--- configs/lede/diy-arm.sh
+++ configs/lede/diy-arm.sh
@@ -75,11 +75,6 @@ rm -rf package/small/sing-box
 
 # Update Go Version
 # rm -rf feeds/packages/lang/golang && git clone -b 22.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
-./scripts/feeds clean
-./scripts/feeds update -a
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a
 
 # dockerd去版本验证
 #sed -i 's/^\s*$[(]call\sEnsureVendoredVersion/#&/' feeds/packages/utils/dockerd/Makefile
