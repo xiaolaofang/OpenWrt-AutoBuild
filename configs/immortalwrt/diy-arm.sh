ace0fd37addd30fd99cf33891049e50d6650f9b5
laobu01 <dfbucy15@gmail.com>
2025-01-11 08:55:40 +0800

    Update diy-arm.sh

--- configs/immortalwrt/diy-arm.sh
+++ configs/immortalwrt/diy-arm.sh
@@ -22,6 +22,8 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 # Enable AAAA
 sed -i 's/filter_aaaa	1/filter_aaaa	0/g' package/network/services/dnsmasq/files/dhcp.conf
 #git clone --depth=1 https://github.com/yunxi993/extra.git package/extra
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # Timezone
 #sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
